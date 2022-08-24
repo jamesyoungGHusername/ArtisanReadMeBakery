027948b7d058323a0abd1f39acf7e62a294a7a82
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-08-24 17:53:37 -0600

    installed inquirer

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer]');
+const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = ["What is the name of your project?","Describe your project.","How should your app be installed?","How should your app be used?","Any notes for the credits section?","What license will you be using?"];
