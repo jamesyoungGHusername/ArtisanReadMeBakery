fbd3c2de0fa8f4b016278473b6d488651c3a966b
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2022-08-25 23:57:01 -0600

    updated comments, app done. TODO: tests, demo video, readme.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const genMd = require('./utils/generateMarkdown.js');
 
+
+const genMd = require('./utils/generateMarkdown.js');
 //Named constant same as class definition.
 const Question = require('./utils/Question.js');
 
@@ -19,12 +20,13 @@ const qTypes = {
     editor: "editor"
 }
 
-// TODO: Create an array of questions for user input
+//  array of questions for user input
 const questions = [
     new Question(qTypes.input,"What is the name of your project?","Title"),
     new Question(qTypes.input,"Describe your project.","Description"),
     new Question(qTypes.input,"How should your app be installed?","Installation"),
     new Question(qTypes.input,"How should your app be used?","Usage"),
+    //The open source initiative has an api, and I considered pulling the full license list for this app, but turns out there are a LOT of open source licenses. The below are the most popular.
     new Question(qTypes.list,"What license will you be using?","License",["MIT License","Apache 2.0","BSD 3-Clause","BSD 2-Clause","GNU GPL","GNU LGPL","Mozilla Public License 2.0","Common Development and Distribution License","Eclipse Public License version 2.0"]),
     new Question(qTypes.input,"Who contributed?","Credits"),
     new Question(qTypes.input,"How should someone contribute?","Contributing"),
@@ -35,14 +37,14 @@ const questions = [
 
 var sections=[]
 
-// TODO: Create a function to write README file
+// function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName,genMd(data),(err)=>
         err ? console.error(err) : console.log('File written.')
     );
 }
 
-// TODO: Create a function to initialize app
+//function to initialize app
 function init() {
     inquirer
     .prompt(questions)
